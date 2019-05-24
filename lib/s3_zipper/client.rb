dc6150492d51ce6d053b720d177c17b14b176061
Nickolas Komarnitsky <komarnickolas@gmail.com>
2019-05-24 15:41:41 -0600

    more verbose output

--- lib/s3_zipper/client.rb
+++ lib/s3_zipper/client.rb
@@ -58,10 +58,10 @@ class S3Zipper
 
     def upload(local_path, repo_path, options: {})
       spinner = TTY::Spinner.new('[:spinner] :title', format: :dots_2)
-      spinner.update(title: "Uploading zip to #{repo_path}")
+      spinner.update(title: "Uploading zip to #{bucket_name}/#{repo_path}")
       spinner.auto_spin
       object = client.put_object(options.merge!(bucket: bucket_name, key: repo_path, body: File.open(local_path).read))
-      spinner.update(title: "Uploaded zip to #{repo_path}")
+      spinner.update(title: "Uploaded zip to #{bucket_name}/#{repo_path}")
       spinner.success
       object
     end
